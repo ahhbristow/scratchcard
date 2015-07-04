09c6b9d08f9bdaf45c05f768ea5d459b115a6755
Shaun Bristow <shaun.bristow@openbet.com>
2015-07-05 00:18:29 +0100

    Fixed bug where window shrinks to right most cards position

--- public/js/directives.js
+++ public/js/directives.js
@@ -20,6 +20,18 @@ cardsApp.directive('draggable', function() {
 				drag:function(e, ui) {
 					scope.$eval(attrs.x + '=' + $(element).offset().left);
 					scope.$eval(attrs.y + '=' + $(element).offset().top);
+
+					// Update the min-width of the cards container to ensure
+					// we don't shrink the view area when moving the furthest
+					// right card back to the left of the screen
+					var container = $(element).parent().parent();
+					var element_x = $(element).offset().left;
+					var current_min_width = parseInt($(container).css('min-width'));
+					console.log("current: " + current_min_width + ", left: " + element_x);
+					if (element_x > current_min_width) {
+						container.css({'min-width' : element_x});
+					}
+
 					scope.$apply(function() {
 						scope.dragMove(scope.card);
 					});
