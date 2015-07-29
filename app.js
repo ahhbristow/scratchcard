ebe3048bf43135c04cbbb683b37f08e0608de5be
Shaun Bristow <shaun.bristow@openbet.com>
2015-07-29 19:50:22 +0100

    Fixed bower install and added Dockerfile mongo environment variable

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 // ====== DB initialise =======
 
 var mongoose = require('mongoose');
-var mongo_uri = process.env.MONGOLAB_URI || "mongodb://localhost/sessions";
+var mongo_uri = process.env.MONGOLAB_URI;
 mongoose.connect(mongo_uri, function(err) {
     if(err) {
         console.log('connection error', err);
