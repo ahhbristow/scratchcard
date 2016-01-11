7b41207a242ce41d4050497e70048a3330e8d391
Shaun Bristow <ahhbristow@hotmail.com>
2016-01-11 19:19:07 +0000

    Adding clicks to <input> elements to focus

--- test/protractor/conf.js
+++ test/protractor/conf.js
@@ -6,20 +6,38 @@ exports.config = {
 
 		// Login as 'master' with first browser	
 		browser.get('http://localhost:4072/');
+		
+		element(by.id('new_username')).click();
 		element(by.id('new_username')).sendKeys("ci_user_master");
+		
+		element(by.id('new_username')).click();
 		element(by.id('new_password')).sendKeys("password");
+
 		element(by.id('register')).click();
+
+		element(by.id('username')).click();
 		element(by.id('username')).sendKeys("ci_user_master");
+		
+		element(by.id('password')).click();
 		element(by.id('password')).sendKeys("password");
+
 		element(by.id('login')).click();
 		
 		// Register and login as 'slave' with second browser
 		browser2.get('http://localhost:4072/');
 		element2 = browser2.element;
+		
+		element2(by.id('new_username')).click();
 		element2(by.id('new_username')).sendKeys("ci_user_slave");
+		
+		element2(by.id('new_password')).click();
 		element2(by.id('new_password')).sendKeys("password");
 		element2(by.id('register')).click();
+
+		element2(by.id('username')).click();
 		element2(by.id('username')).sendKeys("ci_user_slave");
+
+		element2(by.id('password')).click();
 		element2(by.id('password')).sendKeys("password");
 		element2(by.id('login')).click();
 	},
