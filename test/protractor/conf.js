bd9fb30a7a345402e61477cae07b92c499e02e5a
Shaun Bristow <ahhbristow@hotmail.com>
2016-01-11 19:02:14 +0000

    Added registration to test run

--- test/protractor/conf.js
+++ test/protractor/conf.js
@@ -6,14 +6,20 @@ exports.config = {
 
 		// Login as 'master' with first browser	
 		browser.get('http://localhost:4072/');
-		element(by.id('username')).sendKeys("automation_user_master");
+		element(by.id('new_username')).sendKeys("ci_user_master");
+		element(by.id('new_password')).sendKeys("password");
+		element(by.id('register')).click();
+		element(by.id('username')).sendKeys("ci_user_master");
 		element(by.id('password')).sendKeys("password");
 		element(by.id('login')).click();
 		
-		// Login as 'slave' with second browser
+		// Register and login as 'slave' with second browser
 		browser2.get('http://localhost:4072/');
 		element2 = browser2.element;
-		element2(by.id('username')).sendKeys("automation_user_slave");
+		element2(by.id('new_username')).sendKeys("ci_user_slave");
+		element2(by.id('new_password')).sendKeys("password");
+		element2(by.id('register')).click();
+		element2(by.id('username')).sendKeys("ci_user_slave");
 		element2(by.id('password')).sendKeys("password");
 		element2(by.id('login')).click();
 	},
