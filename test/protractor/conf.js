6f5bfb4f29f2b5f42a74cf41679c9984dbdde5e5
Shaun Bristow <ahhbristow@hotmail.com>
2016-01-12 10:40:11 +0000

    Added browser maximising

--- test/protractor/conf.js
+++ test/protractor/conf.js
@@ -3,6 +3,7 @@ var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
 exports.config = {
 	specs: ['test.js'],
 	onPrepare: function() {
+
 		jasmine.getEnv().addReporter(
 			new HtmlScreenshotReporter({
 				dest: '$CIRCLE_ARTIFACTS',
@@ -12,6 +13,8 @@ exports.config = {
 
 		// Fork a new browser instance
 		browser2 = browser.forkNewDriverInstance();
+		browser.driver.manage().window().maximize();
+		browser2.driver.manage().window().maximize();
 
 		// Login as 'master' with first browser	
 		browser.get('http://localhost:4072/');
@@ -23,6 +26,7 @@ exports.config = {
 		element(by.id('new_password')).sendKeys("password");
 
 		element(by.id('register')).click();
+		console.log('Registered in master browser');
 
 		element(by.id('username')).click();
 		element(by.id('username')).sendKeys("ci_user_master");
@@ -31,6 +35,7 @@ exports.config = {
 		element(by.id('password')).sendKeys("password");
 
 		element(by.id('login')).click();
+		console.log('Logged into master browser');
 
 		// Register and login as 'slave' with second browser
 		browser2.get('http://localhost:4072/');
@@ -42,6 +47,7 @@ exports.config = {
 		element2(by.id('new_password')).click();
 		element2(by.id('new_password')).sendKeys("password");
 		element2(by.id('register')).click();
+		console.log('Registered in slave browser');
 
 		element2(by.id('username')).click();
 		element2(by.id('username')).sendKeys("ci_user_slave");
@@ -49,6 +55,7 @@ exports.config = {
 		element2(by.id('password')).click();
 		element2(by.id('password')).sendKeys("password");
 		element2(by.id('login')).click();
+		console.log('Logged into slave browser');
 	},
 	onComplete: function(exitCode) {
 		browser2.quit();
